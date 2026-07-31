c4c38c9d91f1cf5240db6598369b9d3701ea493f
TimmbulIlusi <hastowawandono@gmail.com>
2026-08-01 04:33:52 +0700

    Fix Vercel serverless storage paths and enable debug

--- api/index.php
+++ api/index.php
@@ -1,3 +1,38 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Vercel Serverless Storage Handler for Laravel
+|--------------------------------------------------------------------------
+| Vercel filesystem is read-only. We must redirect storage and cache
+| paths to the writable /tmp directory.
+*/
+
+$storageDirs = [
+    '/tmp/storage/app',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/logs',
+    '/tmp/bootstrap/cache',
+];
+
+foreach ($storageDirs as $dir) {
+    if (!file_exists($dir)) {
+        @mkdir($dir, 0755, true);
+    }
+}
+
+putenv('APP_STORAGE=/tmp/storage');
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+putenv('SESSION_DRIVER=array');
+putenv('LOG_CHANNEL=stderr');
+putenv('CACHE_STORE=array');
+
+$_ENV['APP_STORAGE'] = '/tmp/storage';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+$_ENV['SESSION_DRIVER'] = 'array';
+$_ENV['LOG_CHANNEL'] = 'stderr';
+$_ENV['CACHE_STORE'] = 'array';
+
 require __DIR__ . '/../public/index.php';
