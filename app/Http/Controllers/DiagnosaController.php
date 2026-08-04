5113a565167cd49c3f5504de9bde1a741f88a090
TimmbulIlusi <hastowawandono@gmail.com>
2026-08-04 11:45:06 +0700

    Final Fix for result page and controller

--- app/Http/Controllers/DiagnosaController.php
+++ app/Http/Controllers/DiagnosaController.php
@@ -8,6 +8,14 @@ use Illuminate\Support\Facades\Log;
 
 class DiagnosaController extends Controller
 {
+    // TAMBAHKAN INI AGAR ERROR HILANG
+    public function dashboard(Request $request) 
+    { 
+        return view('dashboard', ['lang' => $request->input('lang', 'id')]); 
+    }
+
+    public function index(Request $request) { return view('index', ['lang' => $request->input('lang', 'id')]); }
+
     public function predict(Request $request)
     {
         $lang = $request->input('lang', 'id');
@@ -18,7 +26,6 @@ class DiagnosaController extends Controller
         }
 
         try {
-            // Memanggil API Python
             $response = Http::timeout(10)->post('https://hasto.pythonanywhere.com/api/predict', [
                 'symptoms' => $selectedSymptoms,
                 'lang' => $lang
@@ -26,8 +33,6 @@ class DiagnosaController extends Controller
 
             if ($response->successful()) {
                 $data = $response->json();
-                
-                // Mengirim data ke view result
                 return view('result', [
                     'top_predictions' => $data['top_predictions'], 
                     'gejala_terpilih' => $data['gejala_terpilih'],
